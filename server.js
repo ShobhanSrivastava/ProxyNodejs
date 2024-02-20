bb036772b88b5b9041cf7854e2f7b49b98b3ca1b
Shobhan Srivastava <srivastavashobhan2001@gmail.com>
2024-02-20 15:04:08 +0530

    Middleware fix

--- server.js
+++ server.js
@@ -6,11 +6,10 @@ const { PORT, TARGET_URL } = process.env;
 
 const app = express();
 
-app.use('/', createProxyMidleware({
-    target: TARGET_URL
+app.use(createProxyMidleware({
+    target: TARGET_URL,
 }));
 
 app.listen(PORT || 8080, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
