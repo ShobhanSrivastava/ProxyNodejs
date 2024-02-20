9f948209f1a6b52e5cfb5da879dbc846a8faba2e
Shobhan Srivastava <shobhansrivastava@Shobhans-MacBook-Air.local>
2024-02-20 14:41:50 +0530

    Proxy

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { createProxyMidleware } = require('http-proxy-middleware');
+require('dotenv').config();
+
+const { PORT, TARGET_URL } = process.env;
+
+const app = express();
+
+app.use('/', createProxyMidleware({
+    target: TARGET_URL
+}));
+
+app.listen(PORT || 8080, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
