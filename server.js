f6ad56f4340b4efb0a5db446a8e1c8515857675d
Shobhan Srivastava <shobhansrivastava@Shobhans-MacBook-Air.local>
2024-02-27 00:45:21 +0530

    ngrok browser warning skip header added

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
-const { createProxyMidleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 require('dotenv').config();
 
 const { PORT, TARGET_URL } = process.env;
 
 const app = express();
 
-app.use('/', createProxyMidleware({
-    target: TARGET_URL
+app.use('/', createProxyMiddleware({
+    target: TARGET_URL,
+    onProxyReq(proxyReq, req, res) {
+        proxyReq.setHeader('ngrok-skip-browser-warning', 'true');
+    }
 }));
 
 app.listen(PORT || 8080, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
